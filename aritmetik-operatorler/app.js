5caf372bbd98a693ff0a41072966a680fa7dbba2
Serap Pala <72605128+SerapPala@users.noreply.github.com>
2023-11-04 12:50:54 +0300

    Add files via upload

---
+++ aritmetik-operatorler/app.js
@@ -0,0 +1,46 @@
+/*
+ARİTMETİK OPERATÖRLER
++
+-
+*
+/
+% 
+++
+--
+**
+
+
+=
+eşitliğin sağ tarafındaki 5 değerini al, eşitliğin sol tarafındaki a içerisine 5 i koy demektir. a 5 e eşittir demek değildir. = ifadesi eşittir demek değildir,
+atama operatörüdür.
+let a = 5
+let isim = "Serap"
+
+//
++ -> string ile number eklemeye calısırsak string ile numberı yan yana koyar, birleştirilir. eklenmez. matematiksel olarak number olmalı toplaması için.
+*/
+
+// +
+console.log(5+6)
+let not1 = 60;
+let not2=70;
+console.log("Notlarınızın toplamı:" + (not1+not2))
+// -
+console.log(not1-not2)
+// *
+console.log(not1*not2)
+// /
+console.log(not1/not2)
+//  % -> > mod alma operatörü, örneğin 15'in 2'ye bölümünden kalanı bulur.
+console.log(15%2)
+// ++ : 1 arttırmak için kullanılır.
+let b = 10
+b++; //11
+b++ //12
+console.log(b)
+// -- : 1 eksiltmek için kullanılır.
+b--;//11
+b--;//10
+console.log(b)
+// * bir sayının üssünü almak için kullanılır.
+console.log(4**2)
