85a9adda0fe4284a038fe06d914d6af0b74ef4fa
Serap Pala <72605128+SerapPala@users.noreply.github.com>
2023-11-04 14:01:42 +0300

    Add files via upload
    koşul yapıları: if elseif else kapsamında yol ayrımı uygulaması

---
+++ yol-ayrimi-uygulamasi-if-elseif-else/app.js
@@ -0,0 +1,13 @@
+/*
+YOL AYRIMI UYGULAMASI -> 2 yol var
+*/
+
+let secilenYol = prompt("Lütfen gitmek istediğiniz yolu seçiniz.")
+
+if(secilenYol == 1 ){
+alert( "Seçilen yol" + secilenYol + " " + ".yoldur");
+}else if (secilenYol == 2){
+    alert( "Seçilen yol" + secilenYol  + " " + "yoldur");
+}else {
+    alert( "Lütfen geçerli bir yol seçiniz");
+}
