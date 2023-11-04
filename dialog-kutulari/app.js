f4ad7385207a326a264371938d3a5bae5d2ecc1c
Serap Pala <72605128+SerapPala@users.noreply.github.com>
2023-11-04 13:29:06 +0300

    Add files via upload
    Dialog Kutuları : 
    alert()
    prompt()
    confirm()
    eklendi.

---
+++ dialog-kutulari/app.js
@@ -0,0 +1,25 @@
+/*
+
+DİALOG KUTULARI
+
+alert() -> uyarı anlamına gelir. 
+prompt()
+confirm()
+
+*/
+
+//window objesi içerisinde yer alırlar.Functionlardır.
+console.log(window)
+
+//alert()
+//alert("serap")
+
+//prompt() -> kullanıcıdan değer almak için kullanılır. Her zaman string döner. String tipini sonradan number tipine döünştürebiliriz. (tür dönüşümü)
+let isim = prompt("İsminizi Giriniz : ")
+console.log(isim)
+
+//confirm() -> true false cevap verir.
+confirm("Devam etmek istediğinize emin misiniz? ")
+
+
+
