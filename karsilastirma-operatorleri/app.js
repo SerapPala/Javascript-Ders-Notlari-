bfcff7333166ef7850c9c9c54f3b48f9bb6efa41
Serap Pala <72605128+SerapPala@users.noreply.github.com>
2023-11-04 13:18:59 +0300

    Add files via upload
    karşılaştırma operatörleri eklendi.

---
+++ karsilastirma-operatorleri/app.js
@@ -0,0 +1,20 @@
+/*
+
+KARŞILAŞTIRMA OPERATORLERİ
+
+== : eşit mi
+!= : eşit değil mi
+> : büyük mü
+< : küçük mü
+>= : büyük eşit mi
+<= küçük eşit mi
+
+*/
+
+console.log(5==5);
+console.log(5!=3);
+console.log(5<6);
+console.log(5>6);
+console.log(5>=6);
+console.log(5<=6);
+
