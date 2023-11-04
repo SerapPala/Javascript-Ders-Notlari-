c9be23e470baa460c75df6ad5006ff2c06b7d79c
Serap Pala <72605128+SerapPala@users.noreply.github.com>
2023-11-04 13:01:58 +0300

    Add files via upload

---
+++ atama-operatorleri/app.js
@@ -0,0 +1,35 @@
+/*
+ATAMA OPERATÖRLERİ
+= 
+== 
+=== 
++=
+-=
+*=
+/=
+%=
+**=
+*/
+
+// =  -> atama operatörüdür eşittir demek değildir. 1 i al a'nın içine koy demektir.
+let a = 1;
+// ==  -> eşittir operatörü tipine bakmadan eşittir olur number string vs olup olmadığına bakmaz.
+console.log(5 == 5);
+// 
+console.log(5 == "5")
+// === -> hem değerlerin hem tiplerin eşit olup olmadğını kontrol eder.
+console.log(5 === "5")
+// +=
+let sayi = 4;
+//sayi = sayi+2;
+sayi +=2
+console.log(sayi)
+// sayi = sayi-1
+sayi -=1;
+console.log(sayi)
+//
+sayi %=2;
+console.log(sayi)
+//
+sayi /=2;
+console.log(sayi)
