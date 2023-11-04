44a10856bcd5bf208808bfce836f10594e818f53
Serap Pala <72605128+SerapPala@users.noreply.github.com>
2023-11-04 13:15:15 +0300

    Add files via upload

---
+++ mantıksal-operatorler/app.js
@@ -0,0 +1,31 @@
+/*
+
+MANTIKSAL OPERATORLER
+
+&& : ve
+|| : veya
+!  : değil
+
+*/
+
+
+// &&
+let yas = 17
+let cuzdan = 1000
+if(yas>18 && cuzdan > 1000){
+    console.log("Doğru");
+}else {
+    console.log("Yanlıs")
+}
+// || 
+if(yas>18 || cuzdan >= 1000){
+    console.log("Doğru");
+}else {
+    console.log("Yanlıs")
+}
+// !
+if(yas>18 || !cuzdan >= 1000){
+    console.log("Doğru");
+}else {
+    console.log("Yanlıs")
+}
