09df76c7fba77acdfc94291fa68d458f0476de8c
Serap Pala <72605128+SerapPala@users.noreply.github.com>
2023-11-04 15:07:33 +0300

    Add files via upload

---
+++ tur-donusumu/app.js
@@ -0,0 +1,39 @@
+
+/* 
+TÜR DÖNÜŞÜMLERİ
+
+VERİ TİPLERİ
+PRIMITIVE(İLKEL)
+1-string
+2-number
+3-booleam
+4-null
+5-undefined
+
+REFERANCE(GELİŞMİŞ)
+1-object
+2-Function
+
+*/ 
+let a= 5;
+let b= "10"
+//String veri tipinden number veri tipine döüştürmel
+let c = Number(b);
+console.log(c)
+
+//Number -> numbera çevirir.
+//patsseInt() -> numbera çevirir
+
+
+//parseFloat (ondaliklı sayiya çevirir.)
+let d= 8;
+let e= parseInt("12")
+console.log(d+e)
+
+//string string tipine dönüştürür
+let x = String(55)
+console.log(typeof x)
+
+//toString
+let y = (55).toString
+console.log(y + typeof y)
